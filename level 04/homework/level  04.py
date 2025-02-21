6c7f5fb5b55e8cd8c59a611fe5fa45817ec4958a
gega869 <gega.dolidze17@gmail.com>
2025-02-21 21:34:02 +0400

    forth commit

---
+++ level 04/homework/level  04.py
@@ -0,0 +1,27 @@
+#1
+
+num1 = ;one;
+print = num1
+#there has the be the symbol "" not the symbol ;;
+
+#2
+
+num2 = ('two")
+ #the mistake is that we used '" too. only thng  we should be using is "" for string   
+
+#3
+
+num3 = "three'    
+ #looks like for this one we have 2 errors. we have to use "" not "' and we typed num3 = "three" over at the print is Num3 n is a capital letter witch can cause an error.
+print(Num3)
+
+#4
+ 
+num4 = "number4    
+ #even if num4 means number4 its not the same in programing. the meaning dosnt matter it has to be the same exact thing num4 has to be num4 not number4 
+print(num4)
+
+#5
+
+5num = 'five'    #i dont undertsand why 5num is red but we have to use "" symbols not '' i think..
+
---
+++ level 04/homework/level 04.py
@@ -0,0 +1,17 @@
+salary = 5000
+pay_raise = 2000
+new_salary = salary+pay_raise
+
+print(new_salary)
+
+
+payment = 7500
+payment_increese = 50
+new_payment = payment+payment_increese
+print(new_payment)
+
+
+number = 423
+second_number = 2443
+total = second_number*number
+print(total)
